53e10d66bf5d1f1fccbbb220341747ef95643222
jeffersonfarias <jeffarbar@yahoo.com.br>
2022-06-06 09:15:29 -0300

    Update Sidebar.vue

--- src/Layout/Components/Sidebar.vue
+++ src/Layout/Components/Sidebar.vue
@@ -82,10 +82,10 @@
                                 href: '/api/score-b2b',
                                 title: 'Antifraude',
                             }
-                            //,{
-                            //    href: '/api/score-simswap',
-                            //    title: 'SIM Swap',
-                            //}
+                            ,{
+                                href: '/api/score-simswap',
+                                title: 'SIM Swap',
+                            }
                         ]
                     },
 
